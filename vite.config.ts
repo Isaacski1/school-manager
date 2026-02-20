50921111a0a138365031d773cf4857466f15a065
Isaac Krampah <isaacskikrams@gmail.com>
2026-02-20 23:19:47 +0000

    Fix the install PWA for production

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import path from "path";
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, ".", "");
@@ -11,7 +12,31 @@ export default defineConfig(({ mode }) => {
       host: "::",
     },
 
-    plugins: [react()],
+    plugins: [
+      react(),
+      VitePWA({
+        registerType: "autoUpdate",
+        includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+        manifest: {
+          name: "School Manager GH",
+          short_name: "School Manager",
+          description: "School Management System",
+          theme_color: "#ffffff",
+          icons: [
+            {
+              src: "pwa-192x192.png",
+              sizes: "192x192",
+              type: "image/png",
+            },
+            {
+              src: "pwa-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
+            },
+          ],
+        },
+      }),
+    ],
 
     resolve: {
       alias: {
